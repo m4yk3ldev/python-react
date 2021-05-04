6b47f9f32104af3b121df870d0245a1e7615a95b
Maykel Arias Torres <hanibal920915@gmail.com>
2021-05-03 23:30:59 -0400

    Activado el cors

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api.models import Movie, Rating
